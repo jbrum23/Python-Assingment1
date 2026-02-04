e4fc9f73037be11ceb041742f31ad8bf444d3c61
jbrum23 <john.brum@student.fairfield.edu>
2026-02-04 01:07:42 +0000

    Exercise 1: Profit Margin Calculator with float inputs.

--- Excercise 1.py
+++ Excercise 1.py
@@ -1,11 +1,15 @@
+# Prompt the user for the product's revenue and cost.
 revenue = float(input("What is the product's revenue? "))
 cost = float(input("What is the product's cost? "))
 
+# calculate profit
 profit = revenue - cost
 
+# calculate margin, ensuring we don't divide by zero
 if revenue > 0:
     margin = (profit / revenue) * 100
 else:
     margin = 0
 
+# Display the profit and margin to the user.
 print(f"The product's profit is: ${profit:.2f} | Margin : {margin:.2f}%") 
